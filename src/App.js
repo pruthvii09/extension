56c16d163d480db40cd67bf310e09f8d6a0d03e0
pruthvii09 <contact.pruthvirajauti@gmail.com>
2023-08-28 23:36:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import { IoEarth } from "react-icons/io5";
+import { GiModernCity } from "react-icons/gi";
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [ipAddress, setIpAddress] = useState("");
+  const [locationInfo, setLocationInfo] = useState({});
+  useEffect(() => {
+    const fetchIpAddress = async () => {
+      try {
+        const response = await fetch("https://api.ipify.org?format=json");
+        const data = await response.json();
+        setIpAddress(data.ip);
+      } catch (error) {
+        console.error("Error fetching IP address:", error);
+      }
+    };
+    fetchIpAddress();
+  }, []);
+  const handleLocation = async () => {
+    setIsLoading(true);
+    const apiKey = process.env.REACT_APP_API_KEY; // Corrected environment variable name
+    try {
+      const response = await fetch(
+        `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAddress}`
+      );
+      const data = await response.json();
+      setLocationInfo({
+        country: data.location.country,
+        city: data.location.city,
+      });
+      console.log(locationInfo);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      console.error("Error fetching location information:", error);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div className="flex items-start justify-start flex-col gap-1">
+        <div className="flex items-center gap-1">
+          <IoEarth className="text-xl text-[#405cf5]" />
+          <h3 className="text-lg ">
+            Country : {isLoading ? "Loading..." : <>{locationInfo.country}</>}
+          </h3>
+        </div>
+        <div className="flex items-center gap-1">
+          <GiModernCity className="text-xl text-[#405cf5]" />
+          <h3 className="text-lg">
+            City : {isLoading ? "Loading..." : <>{locationInfo.city}</>}
+          </h3>
+        </div>
+        <button
+          className="p-2 text-xs bg-[#405cf5] rounded-md text-white cursor-pointer"
+          onClick={handleLocation}
         >
-          Learn React
-        </a>
-      </header>
+          Get Location
+        </button>
+      </div>
     </div>
   );
 }
